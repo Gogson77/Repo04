85793a1a65f932c4ece75930b976943d6f90a13b
Gordan Kranjcic <gordankranjcic@gmail.com>
2021-09-17 02:05:31 +0200

    Day04ex01-15

---
+++ ex10/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+    // only change code below this line 
+    var myFirstName = "Gordan";
+    var myLastName = "Kranjcic";
+    var myHomeTown = "Sarajevo";
+    // only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+console.log(main());
+module.exports = main;
